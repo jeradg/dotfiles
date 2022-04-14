736fc0c2f759f627a303f77d45bc368cb9df81dc
Jerad Gallinger <jerad.gallinger@shopify.com>
2022-04-14 15:47:13 -0400

    Tweak bootstrap.sh; install neovim/SpaceVim dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+export JERADG_DOTFILES_PATH="$(pwd)"
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-mv ~/.zshrc ~/.zshrc_OLD
+mv ~/.zshrc ~/.zshrc.default-oh-my-zsh
 
 # link dotfiles
-ln -s "$(pwd)/.SpaceVim.d" ~/.SpaceVim.d
-ln -s "$(pwd)/.tmux/.tmux.conf" ~/.tmux.conf
-ln -s "$(pwd)/.zsh/.zshrc" ~/.zshrc
+ln -s "$(JERADG_DOTFILES_PATH)/.SpaceVim.d" ~/.SpaceVim.d
+ln -s "$(JERADG_DOTFILES_PATH)/.tmux/.tmux.conf" ~/.tmux.conf
+ln -s "$(JERADG_DOTFILES_PATH)/.zsh/.zshrc" ~/.zshrc
+
+# Install neovim/SpaceVim dependencies
+
+python3 -m pip install --user --upgrade pynvim
+gem install neovim
+yarn global add neovim
+yarn global add tree-sitter-cli
