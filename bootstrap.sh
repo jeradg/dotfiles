ad6a2c740d1be316dd52e5dbcef4754671c13182
Jerad Gallinger <jerad.gallinger@shopify.com>
2022-04-14 18:08:49 -0400

    Change env-specific tmux symlink location

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,20 +5,24 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 mv ~/.zshrc ~/.zshrc.default-oh-my-zsh
 
-# link dotfiles
-ln -s $(pwd)/.tmux/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/.zshrc ~/.zshrc
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
 
 case "$OSTYPE" in
   darwin*)
-    $ENV_PREFIX="darwin"
+    ENV_PREFIX="darwin"
   ;;
   linux*)
-    $ENV_PREFIX="linux"
+    ENV_PREFIX="linux"
   ;;
 esac
 
-ln -s $(pwd)/.tmux/env/$ENV_PREFIX.conf $(pwd)/.tmux/env.conf
+# link dotfiles
+ln -sf $SCRIPTPATH/.tmux/.tmux.conf ~/.tmux.conf
+ln -sf $SCRIPTPATH/.tmux/env/$ENV_PREFIX.conf ~/.tmux.env.conf
+ln -sf $SCRIPTPATH/.zshrc ~/.zshrc
 
 # Install neovim/SpaceVim dependencies
 
